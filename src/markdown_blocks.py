fcce108139cc3045963973f01a96f9d64e8fba9b
Maciej Sieradz <macieks53@gmail.com>
2025-01-13 10:29:56 +0100

    Added method for returning type of markdown block

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,5 +1,11 @@
 from typing import List
 
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_olist = "ordered_list"
+block_type_ulist = "unordered_list"
 
 def markdown_to_blocks(markdown: str) -> List[str]:
 
@@ -11,3 +17,34 @@ def markdown_to_blocks(markdown: str) -> List[str]:
             filtered_blocks.append(block)
 
     return filtered_blocks
+
+def block_to_block_type(block: str) -> str:
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return block_type_heading
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return block_type_code
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return block_type_paragraph
+        return block_type_quote
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return block_type_paragraph
+        return block_type_ulist
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return block_type_paragraph
+        return block_type_ulist
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return block_type_paragraph
+            i += 1
+        return block_type_olist
+    return block_type_paragraph
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import block_to_block_type, markdown_to_blocks
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -45,6 +45,32 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
+
+
+    def test_block_to_block_type_heading(self):
+        self.assertEqual("heading", block_to_block_type("# This is a heading"))
+
+    def test_block_to_block_type_code(self):
+        self.assertEqual("code", block_to_block_type("```\nThis is the code\n```"))
+
+    def test_block_to_block_type_quote(self):
+        self.assertEqual("quote", block_to_block_type(">quote\n>quote\n>quote"))
+
+    def test_block_to_block_type_list_1(self):
+        block = "* one\n* two\n* three"
+        self.assertEqual("unordered_list", block_to_block_type(block))
+
+    def test_block_to_block_type_list_2(self):
+        block = "- one\n- two\n- three"
+        self.assertEqual("unordered_list", block_to_block_type(block))
+
+    def test_block_to_block_type_list_3(self):
+        block = "1. one\n2. two\n3. three"
+        self.assertEqual("ordered_list", block_to_block_type(block))
+
+    def test_block_to_block_type_paragraph(self):
+        self.assertEqual("paragraph", block_to_block_type("This is standard paragraph\n. Parapraph."))
+
 
 
 if __name__ == "__main__":
